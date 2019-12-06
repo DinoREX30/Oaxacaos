54dd9222d90c61e74c5905646f69209e03dad6a3
Cinthya <cint_280@outlook.com>
2019-12-05 18:46:19 -0600

    Código firebase

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
-Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js"></script>
+
+
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBdO5KoqEhDtUGG1n8OrXVJP1N5cRVOPho",
+    authDomain: "oaxcaosor.firebaseapp.com",
+    databaseURL: "https://oaxcaosor.firebaseio.com",
+    projectId: "oaxcaosor",
+    storageBucket: "oaxcaosor.appspot.com",
+    messagingSenderId: "354344247448",
+    appId: "1:354344247448:web:c7010a4221a0825d39f0ed",
+    measurementId: "G-4CLFQ7K2X0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
